162c9c3c49d69eb981c575297f49a9d16c63b3c6
oljar <oljar@tlen.pl>
2021-11-02 05:39:36 +0100

    first commit

--- EVOT_printer.py
+++ EVOT_printer.py
@@ -1,15 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+from tkinter import ttk
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+window = Tk()
+window.title("Welcome to TutorialsPoint")
+window.geometry('400x400')
+a = Label(window ,text = "First Name").grid(row = 0,column = 0)
+b = Label(window ,text = "Last Name").grid(row = 1,column = 0)
+c = Label(window ,text = "Email Id").grid(row = 2,column = 0)
+d = Label(window ,text = "Contact Number").grid(row = 3,column = 0)
+a1 = ttk.Entry(window).grid(row = 0,column = 1)
+b1 = Entry(window).grid(row = 1,column = 1)
+c1 = Entry(window).grid(row = 2,column = 1)
+d1 = Entry(window).grid(row = 3,column = 1)
+
+selected_month = StringVar()
+
+months = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+month_cb = ttk.Combobox(window , values = months ,textvariable=selected_month ).grid(row=4,column = 4)
+
+print(selected_month.get())
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def clicked():
+    print(selected_month.get())
 
+
+btn = Button(window ,text="Submit",command= clicked).grid(row=4,column=0)
+
+
+window.mainloop()

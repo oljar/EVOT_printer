4e1795140b39a99366c2ed16857fe060f458f5b6
oljar <oljar@tlen.pl>
2022-01-01 14:25:01 +0100

    s&p

--- EVOT_printer.py
+++ EVOT_printer.py
@@ -1175,7 +1175,7 @@ class Application (Frame):
                         'Pole tekstowe 4': self.selected_exhaust_value.get() + self.selected_exhaust_execution_value.get(), 'Pole tekstowe 5': self.entry_supply_flow_value.get() + '\r' + self.entry_exhaust_flow_value.get(),
                         'Pole tekstowe 6': self.entry_supply_pressure_value.get()+'\r'+self.entry_exhaust_pressure_value.get(),'Pole tekstowe 7': '','Pole tekstowe 8': '',
                          'Pole tekstowe 9': '', 'Pole tekstowe 10': '', 'Pole tekstowe 11': '', 'Pole tekstowe 12': '', 'Pole tekstowe 16': '',
-                         'Pole tekstowe 17': 'S&P', 'Pole tekstowe 18': 'Data prod', 'Pole tekstowe 19': 'projekt', 'Pole tekstowe 23': '', 'Pole tekstowe 24': '', 'Pole tekstowe 25': '',
+                         'Pole tekstowe 17': '', 'Pole tekstowe 18': 'Data prod', 'Pole tekstowe 19': 'projekt', 'Pole tekstowe 23': '', 'Pole tekstowe 24': '', 'Pole tekstowe 25': '',
                          'Pole tekstowe 26': 'system AHU','Pole tekstowe 27': '', 'Pole tekstowe 28': '', 'Pole tekstowe 29': 'Nazwisko kontrolera'}
 
 
@@ -1378,7 +1378,7 @@ class Application (Frame):
 #### heat_recovery
 
         if self.heat_exchanger_choice_value.get() == 1 :
-            if self.damper_choice_value.get() == 1 or self.damper_choice_value.get() == 2 or self.damper_choice_value.get() == 3:
+            if self.damper_choice_value.get() == 1 or self.damper_choice_value.get() == 2 or self.damper_choice_value.get() == 3 or self.add_equip_choice_value.get() == 1:
                 self.add_line = ''
             else:
                 self.add_line = '\r'
@@ -1411,7 +1411,20 @@ class Application (Frame):
             data_tabliczka_out['sec filt 4 e'] = ' ' + self.symbol_exaust_damper_value.get()
             data_tabliczka_out['sec filt 4 s'] =  ' ' + self.symbol_supply_damper_value.get()
             data_tabliczka_out['sec filt 4'] = 'Tłumik'
-            data_atest_out['Pole tekstowe 16'] = data_atest_out['Pole tekstowe 16'] + '\r'+ ' ' + self.symbol_supply_damper_value.get() + '\r'+ ' ' + self.symbol_exaust_damper_value.get()
+            data_atest_out['Pole tekstowe 16'] = data_atest_out['Pole tekstowe 16'] + '\r'+ ' ' + self.symbol_supply_damper_value.get() + ' ; ' + self.symbol_exaust_damper_value.get()
+
+#   additionals
+
+        if self.add_equip_choice_value.get() == 1:
+            data_atest_out['Pole tekstowe 16'] = data_atest_out['Pole tekstowe 16'] + '\r' + ' '+ self.txt_additional_equipment.get("1.0",END)
+
+        elif self.add_equip_choice_value.get() == 2:
+            data_atest_out['Pole tekstowe 16'] = data_atest_out['Pole tekstowe 16']
+
+ #   S&P
+
+        data_atest_out['Pole tekstowe 17'] = self.txt_s_and_p.get("1.0",END)
+
 
 
 

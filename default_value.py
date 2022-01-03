19f492eb4adc01f95e7c9e92e52ea96603a766ac
Jarosław Olszewski <jolszewski@klimor.com>
2022-01-03 06:47:41 +0100

    pattern 01

--- default_value.py
+++ default_value.py
@@ -1,2 +1,4 @@
-default_path_in = 'C:\\Users\oljar\Desktop\EVOT_printer'
-default_path_out = 'C:\\Users\oljar\Desktop\CV'
+#default_path_in = 'C:\\Users\oljar\Desktop\EVOT_printer'
+#default_path_out = 'C:\\Users\oljar\Desktop\CV'
+default_path_in = 'C:\\Users\jolszewski\\OneDrive - KTGI\Desktop\\templates_EVO'
+default_path_out = 'C:\\Users\jolszewski\\OneDrive - KTGI\Desktop\\work_EVO'

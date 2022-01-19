dd7510b5b93296c1fa31d4473cb8b74a1f4c4e3e
Jarosław Olszewski <jolszewski@klimor.com>
2022-01-19 14:10:39 +0100

    rotor fix filter bug

--- filter_source.py
+++ filter_source.py
@@ -4,7 +4,7 @@ symbol_M5_supply_filter_value = {'1200':'915x305x50','9200':'915x425x50','8000':
 quantity_M5_supply_filter_value = 1
 symbol_F7_supply_filter_value = {'1200':'897x287x75','9200':'897x407x75','8000':'437x287x75','5000':'303x305x75','4100':'592x287x75','3000':'255x305x75'}
 quantity_F7_supply_filter_value = 1
-symbol_F9_supply_filter_value = {'1200':'l12xh12F79sup','9200':'l8xh128sup','8000':'l8xh8g4sup','5000':'l5xh5f9sup','4100':'l41xh41f9sup', '3000':'l30xh30f9sup'}
+symbol_F9_supply_filter_value = {'1200':'l12xh12F79sup','9200':'l8xh128sup','8000':'l8xh8g4sup','5000':'l5xh5f9sup','4100':'592x287x96', '3000':'l30xh30f9sup'}
 quantity_F9_supply_filter_value = 1
 
 symbol_G4_exhaust_filter_value = {'1200':'915x305x50','9200':'915x425x50','8000':'455x305x50','5000':'303x305x50','4100':'610x305x50','3000':'255x305x50'}
@@ -13,6 +13,6 @@ symbol_M5_exhaust_filter_value = {'1200':'915x305x50','9200':'915x425x50','8000'
 quantity_M5_exhaust_filter_value = 1
 symbol_F7_exhaust_filter_value = {'1200':'897x287x75','9200':'897x407x75','8000':'437x287x75','5000':'303x305x75','4100':'592x287x75','3000':'255x305x75'}
 quantity_F7_exhaust_filter_value = 1
-symbol_F9_exhaust_filter_value = {'1200':'l12xh12f9exh','9200':'l8xh8f9exh','8000':'l8xh8g4exh','5000':'l5xh5f9exh','4100':'l41xh41f9exh','3000':'l30xh30f9exh'}
+symbol_F9_exhaust_filter_value = {'1200':'l12xh12f9exh','9200':'l8xh8f9exh','8000':'l8xh8g4exh','5000':'l5xh5f9exh','4100':'592x287x96','3000':'l30xh30f9exh'}
 quantity_F9_exhaust_filter_value = 1
 
